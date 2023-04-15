97d2e2ecf31bf322744f3d844d46730ab6419c9a
dhrubooou <117100651+dhrubooou@users.noreply.github.com>
2023-04-15 16:01:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("HIII");
 //6cfe3ffa84a84af3b8f63697a893b038 ----> api key
 // grab the news container
 let source = 'bbc-news';
@@ -6,7 +5,7 @@ let apikey = '6cfe3ffa84a84af3b8f63697a893b038';
 let News1 = document.getElementById('News1');
 // create a get request
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=6cfe3ffa84a84af3b8f63697a893b038`, true);
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apikey}`, true);
 
 xhr.onload = function () {
     if (this.status === 200) {
@@ -15,25 +14,26 @@ xhr.onload = function () {
         let articles = json.articles;
         let newsHtml = "";
         // console.log(articles);
-        articles.forEach(function(element) {
+        articles.forEach(function (element, index) {
+            console.log(index);
             let news = `<div class="card">
-                            <div class="card-header" id="News1">
+                            <div class="card-header" id="heading${index}">
                                 <h2 class="mb-0">
-                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
-                                        aria-expanded="true" aria-controls="collapseOne">
-                                        ${element.title}
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
+                                    <b>Beaking News ${index + 1}.      </b>${element.title}
                                     </button>
                                 </h2>
                             </div>
-
-                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-                                data-parent="#accordionExample">
-                                <div class="card-body">${element.content}. <a href="${element.url}">Read more here </a></div>
+            
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#News1">
+                                <div class="card-body">${element.content}. <a href="${element.url}" target = "_blank">Read more here </a>
+                                </div>
+                            </div>
                         </div>`
             newsHtml += news;
-                    
+
         });
-        News1.innerHTML=newsHtml;
+        News1.innerHTML = newsHtml;
     }
     else {
         console.log("Some error occured")
